08f3b3e035df7889bf77d30e50ccfe24d26d5d4b
Maxim Bekurin <BekurinMD@yandex.ru>
2022-05-26 13:25:42 +0500

    version 2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,73 @@
-# TODO:  Напишите свой вариант
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Follow, Group, Post, User
+from rest_framework import filters, mixins, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CreateListRetrieveViewSet(mixins.CreateModelMixin,
+                                mixins.ListModelMixin,
+                                viewsets.GenericViewSet):
+    pass
+
+
+class FollowViewSet(CreateListRetrieveViewSet):
+    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого поста запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        serializer.save(author=self.request.user, post=post)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого комментария запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return post.comments.all()
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,94 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200,
+                             verbose_name='Жанр',
+                             help_text='Укажите жанр')
+    slug = models.SlugField(max_length=255,
+                            unique=True,
+                            verbose_name='Параметр',
+                            help_text='Адрес')
+    description = models.TextField(verbose_name='Содержание',
+                                   help_text='Содержание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+    text = models.TextField(verbose_name='Текст поста',
+                            help_text='Введите текст поста')
+    pub_date = models.DateTimeField(verbose_name='Дата публикации',
+                                    auto_now_add=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts',
+                               verbose_name='Автор')
+    image = models.ImageField(verbose_name='Картинка',
+                              upload_to='posts/',
+                              blank=True,
+                              help_text='Картинка')
+    group = models.ForeignKey('Group',
+                              blank=True,
+                              null=True,
+                              on_delete=models.SET_NULL,
+                              verbose_name='Группа',
+                              related_name="posts",
+                              help_text='Группа, относительно поста')
 
     def __str__(self):
         return self.text
 
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='comments',
+                               verbose_name='Автор')
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             verbose_name='Текст поста',
+                             related_name='comments',
+                             blank=True,
+                             null=True)
+    text = models.TextField(verbose_name='Комментарий',
+                            help_text='Напишите комментарий')
+    created = models.DateTimeField(verbose_name='Дата добавления',
+                                   auto_now_add=True,
+                                   db_index=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.PROTECT,
+                             related_name='follower',
+                             verbose_name='Подписчик')
+    following = models.ForeignKey(User,
+                                  on_delete=models.PROTECT,
+                                  related_name='following',
+                                  verbose_name='Подписка')
+
+    def __str__(self):
+        return f'{self.user} follows {self.following}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [models.UniqueConstraint(fields=['user', 'following'],
+                                               name='unique_follow')]
